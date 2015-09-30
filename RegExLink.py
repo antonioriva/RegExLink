fe1284fc94878667f480aec8779717050e6d4645
mom1 <mstolpasov@gmail.com>
2015-09-30 17:42:12 +0300

    Add - regex_link_outlines from issue - current execute folder = folder project

--- RegExLink.py
+++ RegExLink.py
@@ -6,21 +6,31 @@ import sublime_plugin
 import webbrowser
 import subprocess
 import shlex
+import os
 
 REGION_NAME = "RegExLink"
 
 class RegExLinkEventCommand(sublime_plugin.EventListener):
 
     def on_activated_async(self, view):
-        self.highlight(view)
+        self._highlight(view)
 
     def on_post_save_async(self, view):
-        self.highlight(view)
+        self._highlight(view)
 
-    def highlight(self, view):
+    def on_load(self, view):
+        self._highlight(view)
+
+    def _highlight(self, view):
         settings = sublime.load_settings("RegExLink.sublime-settings")
         regex_link_def = settings.get('regex_link_def')
         regex_link_mark_style = settings.get('regex_link_mark_style')
+        regex_link_outlines = settings.get('regex_link_outlines', False)
+
+        if not regex_link_outlines:
+            for link_def in regex_link_def:
+                view.erase_regions(REGION_NAME + link_def['name'])
+            return
 
         for link_def in regex_link_def:
             if 'link' in link_def:
@@ -44,7 +54,9 @@ class RegExLinkCommand(sublime_plugin.TextCommand):
     def run(self, edit):
         settings = sublime.load_settings("RegExLink.sublime-settings")
         regex_link_def = settings.get('regex_link_def')
-
+        currfolder = sublime.expand_variables(
+            "$folder", sublime.active_window().extract_variables())
+        os.chdir(currfolder)
         for link_def in regex_link_def:
             extract = []
 
