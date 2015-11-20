3c2b983ac2b42b69953a2ce1ffc31fb57d0e025a
mom1 <mstolpasov@gmail.com>
2015-11-20 14:13:10 +0300

    BugFix  - I tried your fork but setting it to false also hides the gutter marks.

--- RegExLink.py
+++ RegExLink.py
@@ -10,6 +10,7 @@ import os
 
 REGION_NAME = "RegExLink"
 
+
 class RegExLinkEventCommand(sublime_plugin.EventListener):
 
     def on_activated_async(self, view):
@@ -27,27 +28,30 @@ class RegExLinkEventCommand(sublime_plugin.EventListener):
         regex_link_mark_style = settings.get('regex_link_mark_style')
         regex_link_outlines = settings.get('regex_link_outlines', False)
 
+        flags = sublime.DRAW_NO_FILL
         if not regex_link_outlines:
-            for link_def in regex_link_def:
-                view.erase_regions(REGION_NAME + link_def['name'])
-            return
+            flags = sublime.HIDDEN
 
         for link_def in regex_link_def:
             if 'link' in link_def:
                 extract = []
-                result = view.find_all(link_def['regex'], 0, link_def['link'], extract)
+                result = view.find_all(
+                    link_def['regex'], 0, link_def['link'], extract)
                 for sel in view.sel():
                     view.add_regions(
-                        REGION_NAME + link_def['name'], result, link_def['style'],
-                        regex_link_mark_style, sublime.DRAW_NO_FILL)
+                        REGION_NAME +
+                        link_def['name'], result, link_def['style'],
+                        regex_link_mark_style, flags)
 
             if 'command' in link_def:
                 extract = []
-                result = view.find_all(link_def['regex'], 0, link_def['command'], extract)
+                result = view.find_all(
+                    link_def['regex'], 0, link_def['command'], extract)
                 for sel in view.sel():
                     view.add_regions(
-                        REGION_NAME + link_def['name'], result, link_def['style'],
-                        regex_link_mark_style, sublime.DRAW_NO_FILL)
+                        REGION_NAME +
+                        link_def['name'], result, link_def['style'],
+                        regex_link_mark_style, flags)
 
 
 class RegExLinkCommand(sublime_plugin.TextCommand):
@@ -69,7 +73,8 @@ class RegExLinkCommand(sublime_plugin.TextCommand):
             else:
                 continue
 
-            result = self.view.find_all(link_def['regex'], 0, link_def[linktype], extract)
+            result = self.view.find_all(
+                link_def['regex'], 0, link_def[linktype], extract)
             for sel in self.view.sel():
                 for region in zip(result, extract):
                     if sel.b >= region[0].a and sel.a <= region[0].b:
@@ -80,7 +85,8 @@ class RegExLinkCommand(sublime_plugin.TextCommand):
                             try:
                                 subprocess.Popen(command)
                             except:
-                                sublime.error_message("Error executing: \n\n" + " ".join(command))
+                                sublime.error_message(
+                                   "Error executing: \n\n" + " ".join(command))
 
     def is_visible(self, paths=None):
         if self.contentmenu != "":
@@ -89,23 +95,25 @@ class RegExLinkCommand(sublime_plugin.TextCommand):
             return False
 
     def description(self):
-            settings = sublime.load_settings("RegExLink.sublime-settings")
-            regex_link_def = settings.get('regex_link_def')
-            for link_def in regex_link_def:
-                extract = []
-                if 'link' in link_def:
-                    linktype = 'link'
-                elif 'command' in link_def:
-                    linktype = 'command'
-                else:
-                    continue
+        settings = sublime.load_settings("RegExLink.sublime-settings")
+        regex_link_def = settings.get('regex_link_def')
+        for link_def in regex_link_def:
+            extract = []
+            if 'link' in link_def:
+                linktype = 'link'
+            elif 'command' in link_def:
+                linktype = 'command'
+            else:
+                continue
 
-                result = self.view.find_all(link_def['regex'], 0, link_def[linktype], extract)
-                for sel in self.view.sel():
-                    for region in zip(result, extract):
-                        if sel.b >= region[0].a and sel.a <= region[0].b:
-                            self.contentmenu = link_def['name'] + " " + self.view.substr(region[0])
-                            return "Open " + self.contentmenu
-                        else:
-                            self.contentmenu = ""
-            return "Open " + self.contentmenu
+            result = self.view.find_all(
+                link_def['regex'], 0, link_def[linktype], extract)
+            for sel in self.view.sel():
+                for region in zip(result, extract):
+                    if sel.b >= region[0].a and sel.a <= region[0].b:
+                        self.contentmenu = link_def[
+                            'name'] + " " + self.view.substr(region[0])
+                        return "Open " + self.contentmenu
+                    else:
+                        self.contentmenu = ""
+        return "Open " + self.contentmenu
